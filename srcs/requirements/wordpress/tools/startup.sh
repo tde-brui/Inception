f3fba5704a08a6991fcea2e9a0fccb8e1de38c40
Tijmen De Bruine <tijmendebruine@Tijmens-MacBook-Pro.local>
2024-11-05 22:18:28 +0100

    switched to debian:bookworm, volume mapping fixed, added second wp user

--- srcs/requirements/wordpress/tools/startup.sh
+++ srcs/requirements/wordpress/tools/startup.sh
@@ -7,5 +7,7 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" --allow-root
 ./wp-cli.phar core install --url=$url --title=Inception --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+./wp-cli.phar user create "$WP_NONADMIN_USER" "$WP_NONADMIN_EMAIL" --role=author --user_pass="$WP_NONADMIN_PASSWORD" --allow-root
+
 
 php-fpm8.2 -F
