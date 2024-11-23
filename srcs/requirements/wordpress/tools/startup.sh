f9436bed97e26e5877fa54dc8f53b320e2489763
tde-brui <tijmendebruinebusiness@gmail.com>
2024-11-23 15:44:10 +0100

    php fpm 7.4 and Debian Bullseye

--- srcs/requirements/wordpress/tools/startup.sh
+++ srcs/requirements/wordpress/tools/startup.sh
@@ -11,5 +11,7 @@ sleep 5
 ./wp-cli.phar core install --url=$url --title=Inception --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 ./wp-cli.phar user create "$WP_NONADMIN_USER" "$WP_NONADMIN_EMAIL" --role=author --user_pass="$WP_NONADMIN_PASSWORD" --allow-root
 
+mkdir -p /run/php
+chmod +x /run/php
 
-php-fpm8.2 -F
+php-fpm7.4 -F
